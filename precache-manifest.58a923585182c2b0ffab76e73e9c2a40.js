fc3de4e7ce9dd64489b8150b83e40b1451f04869
huang <huang@huangdeiMac.local>
2020-12-11 18:32:36 -0800

    Updates

---
+++ precache-manifest.58a923585182c2b0ffab76e73e9c2a40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1d870a605ef5f8e18f03ec1ad0a37e",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "e4f9368cff573c3a2c6b",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.021f0fe5.chunk.css"
+  },
+  {
+    "revision": "9f44c9d2b4bd04020615",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.a3408d51.chunk.js"
+  },
+  {
+    "revision": "e4f9368cff573c3a2c6b",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.d56f9b50.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "752ba0aaf7175264e6d589b8b8b22cde",
+    "url": "/seainfo6150-final-project-webapp/static/media/1607637382242.752ba0aa.jpg"
+  },
+  {
+    "revision": "817e64d2d4ad7de098b9c3705a2d897c",
+    "url": "/seainfo6150-final-project-webapp/static/media/healthy-eating.817e64d2.png"
+  },
+  {
+    "revision": "c472097e728260135f66915349c9ce41",
+    "url": "/seainfo6150-final-project-webapp/static/media/makephotogallery.net_1607595311.c472097e.png"
+  },
+  {
+    "revision": "6ca265dc450ebccd326aecaf1aeb07e2",
+    "url": "/seainfo6150-final-project-webapp/static/media/meat.6ca265dc.png"
+  },
+  {
+    "revision": "efbead742fab4ff35c1015ae1123bf4e",
+    "url": "/seainfo6150-final-project-webapp/static/media/pizza.efbead74.png"
+  },
+  {
+    "revision": "939bd453410493df519a48d48a1acd8d",
+    "url": "/seainfo6150-final-project-webapp/static/media/ramen.939bd453.png"
+  }
+]);
